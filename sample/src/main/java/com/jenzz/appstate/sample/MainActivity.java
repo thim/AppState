ed29fa3b9115be16de05fb9f27526057540d9311
Jens Driller <jens@jensdriller.com>
2016-05-12 21:51:34 +0100

    Update sample to show how to pause/resume monitoring

--- sample/src/main/java/com/jenzz/appstate/sample/MainActivity.java
+++ sample/src/main/java/com/jenzz/appstate/sample/MainActivity.java
@@ -1,33 +1,65 @@
 package com.jenzz.appstate.sample;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import com.jenzz.appstate.AppState;
+import android.view.View;
+import android.widget.ImageView;
 import com.jenzz.appstate.RxAppState;
-import rx.Subscription;
-import rx.functions.Action1;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static android.content.Intent.ACTION_GET_CONTENT;
 
 public class MainActivity extends AppCompatActivity {
 
-  private Subscription subscription;
+  private static final int RESULT_CODE_PICK_IMAGE = 1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    subscription = RxAppState.monitor(getApplication())
-        .subscribe(new Action1<AppState>() {
-          @Override
-          public void call(AppState appState) {
-            // Hocus, Pocus, Abracadabra!
-          }
-        });
+    View pickImageButton = findViewById(R.id.pick_image);
+    //noinspection ConstantConditions
+    pickImageButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        getAppState().stopMonitoring();
+        pickImage();
+      }
+    });
   }
 
   @Override
-  protected void onDestroy() {
-    subscription.unsubscribe();
-    super.onDestroy();
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    getAppState().startMonitoring();
+    if (resultCode == RESULT_OK && requestCode == RESULT_CODE_PICK_IMAGE) {
+      showImage(data.getData());
+    }
   }
+
+  private void pickImage() {
+    Intent intent = new Intent(ACTION_GET_CONTENT);
+    intent.setType("image/*");
+    startActivityForResult(intent, RESULT_CODE_PICK_IMAGE);
+  }
+
+  private void showImage(Uri selectedImage) {
+    try {
+      InputStream imageStream = getContentResolver().openInputStream(selectedImage);
+      Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+      ImageView imageView = (ImageView) findViewById(R.id.image_result);
+      //noinspection ConstantConditions
+      imageView.setImageBitmap(bitmap);
+    } catch (FileNotFoundException e) {
+      // Ouch!
+    }
+  }
+
+  private RxAppState getAppState() {return ((SampleApplication) getApplication()).getAppState();}
 }
--- sample/src/main/java/com/jenzz/appstate/sample/SampleApplication.java
+++ sample/src/main/java/com/jenzz/appstate/sample/SampleApplication.java
@@ -1,7 +1,6 @@
 package com.jenzz.appstate.sample;
 
 import android.app.Application;
-import android.util.Log;
 import android.widget.Toast;
 import com.jenzz.appstate.AppState;
 import com.jenzz.appstate.AppStateListener;
@@ -9,37 +8,45 @@ import com.jenzz.appstate.RxAppState;
 import rx.functions.Action1;
 
 import static android.widget.Toast.LENGTH_LONG;
+import static com.jenzz.appstate.AppState.BACKGROUND;
+import static com.jenzz.appstate.AppState.FOREGROUND;
 
 public class SampleApplication extends Application {
 
-  private static final String TAG = SampleApplication.class.getSimpleName();
+  private RxAppState appState;
 
   @Override
   public void onCreate() {
     super.onCreate();
 
-    // Reactive sample
+    // RX sample
     RxAppState.monitor(this).subscribe(new Action1<AppState>() {
       @Override
       public void call(AppState appState) {
-        Log.d(TAG, "RxAppState: " + appState.name());
-        Toast.makeText(SampleApplication.this, "RxAppState: " + appState.name(), LENGTH_LONG).show();
+        // Hocus, Pocus, Abracadabra!
       }
     });
 
     // Callback sample
-    RxAppState appState = RxAppState.create(this);
-    appState.addListener(new AppStateListener() {
-      @Override
-      public void onAppDidEnterForeground() {
-        Log.d(TAG, "RxAppState: onAppDidEnterForeground");
-      }
+    appState = RxAppState.create(this)
+        .addListener(new SampleAppStateListener())
+        .startMonitoring();
+  }
 
-      @Override
-      public void onAppDidEnterBackground() {
-        Log.d(TAG, "RxAppState: onAppDidEnterBackground");
-      }
-    });
-    appState.startMonitoring();
+  public RxAppState getAppState() {
+    return appState;
+  }
+
+  private class SampleAppStateListener implements AppStateListener {
+
+    @Override
+    public void onAppDidEnterForeground() {
+      Toast.makeText(SampleApplication.this, "RxAppState: " + FOREGROUND, LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onAppDidEnterBackground() {
+      Toast.makeText(SampleApplication.this, "RxAppState: " + BACKGROUND, LENGTH_LONG).show();
+    }
   }
 }
