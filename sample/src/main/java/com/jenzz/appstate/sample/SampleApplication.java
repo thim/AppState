8661a20ed8730484f3a195d8d80e5487e511fa0c
Jens Driller <jens@jensdriller.com>
2016-11-12 16:16:57 +0000

    Add logging for app state changes

--- sample/src/main/java/com/jenzz/appstate/sample/SampleApplication.java
+++ sample/src/main/java/com/jenzz/appstate/sample/SampleApplication.java
@@ -1,10 +1,13 @@
 package com.jenzz.appstate.sample;
 
 import android.app.Application;
+import android.util.Log;
 import android.widget.Toast;
+
 import com.jenzz.appstate.AppState;
 import com.jenzz.appstate.AppStateListener;
 import com.jenzz.appstate.RxAppState;
+
 import rx.functions.Action1;
 
 import static android.widget.Toast.LENGTH_LONG;
@@ -12,6 +15,8 @@ import static com.jenzz.appstate.AppState.BACKGROUND;
 import static com.jenzz.appstate.AppState.FOREGROUND;
 
 public class SampleApplication extends Application {
+
+  private static final String TAG = "RxAppState";
 
   private RxAppState appState;
 
@@ -41,12 +46,17 @@ public class SampleApplication extends Application {
 
     @Override
     public void onAppDidEnterForeground() {
-      Toast.makeText(SampleApplication.this, "RxAppState: " + FOREGROUND, LENGTH_LONG).show();
+      logAndToast(FOREGROUND);
     }
 
     @Override
     public void onAppDidEnterBackground() {
-      Toast.makeText(SampleApplication.this, "RxAppState: " + BACKGROUND, LENGTH_LONG).show();
+      logAndToast(BACKGROUND);
     }
+  }
+
+  private void logAndToast(AppState appState) {
+    Log.d(TAG, appState.toString());
+    Toast.makeText(SampleApplication.this, TAG + ": " + appState, LENGTH_LONG).show();
   }
 }
